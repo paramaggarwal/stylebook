60090370a2f243b05a3e544a3e568d85e058406c
Param Aggarwal <paramaggarwal@gmail.com>
2015-04-25 16:08:23 +0530

    view feed

--- index.ios.js
+++ index.ios.js
@@ -14,8 +14,10 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
   TabBarIOS,
-  NavigatorIOS
+  NavigatorIOS,
+  ListView
 } = React;
 
 var SMXTabBarIOS = require('SMXTabBarIOS');
@@ -47,6 +49,7 @@ var Stylebook = React.createClass({
               title: 'My View Title',
               passProps: { myProp: 'foo' },
             }}
+            style={styles.fullScreen}
           />
         </SMXTabBarItemIOS>
         <SMXTabBarItemIOS
@@ -65,6 +68,7 @@ var Stylebook = React.createClass({
               title: 'My View Title',
               passProps: { myProp: 'foo' },
             }}
+            style={styles.fullScreen}
           />
         </SMXTabBarItemIOS>
         <SMXTabBarItemIOS
@@ -83,6 +87,7 @@ var Stylebook = React.createClass({
               title: 'My View Title',
               passProps: { myProp: 'foo' },
             }}
+            style={styles.fullScreen}
           />
         </SMXTabBarItemIOS>
       </SMXTabBarIOS>);
@@ -90,46 +95,113 @@ var Stylebook = React.createClass({
 });
 
 var StyleView = React.createClass({
+  getInitialState: function () {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    return {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  },
+
+  renderRow: function (rowData) {
+    return (
+      <View style={styles.feedCard}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column'
+        }}>
+          <Image source={{
+            uri: 'http://assets.myntassets.com/h_307,q_95,w_230/v1/image/style/properties/587216/Roadster-Men-Navy-Sutil-Solid-Shelby-Slim-Fit-Casual-Shirt_1_4bade0ed34f6e5ca0bbadbcd864badb0_mini.jpg'
+          }} style={styles.productImage} />
+          <Image source={{
+            uri: 'http://assets.myntassets.com/h_307,q_95,w_230/v1/images/style/properties/Roadster-Men-Light-Grey-Corvette-Slim-Fit-Jeans_d8ef5e352f2eaa6d45135ddf274950c6_images_mini.jpg'
+          }} style={styles.productImage} />
+        </View>
+        <View style={{
+          flex: 1,
+        }}>
+          <View style={{
+            alignItems: 'center'
+          }}>
+            <Image source={{
+              uri: 'http://organicthemes.com/demo/profile/files/2012/12/profile_img.png'
+            }} style={{
+              borderRadius: 20,
+              height: 40,
+              width: 40
+            }} />
+            </View>
+          <Text style={styles.welcome}>John Doe</Text>
+          <Text style={styles.tags}>Casual Wear</Text>
+          <Text style={styles.instructions}>Goes great on Monday mornings. The dark shade of the shirt contrasts really well with the light blue jeans.</Text>
+          <Icon
+            name='ion|ios-heart-outline'
+            size={44}
+            color='#FF3A2D'
+            style={{flex: 1, height: 44}}
+          />
+        </View>
+      </View>
+    );
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-        <Icon
-          name='ion|beer'
-          size={150}
-          color='#887700'
-          style={styles.beer}
-        />
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        contentContainerStyle={styles.container}
+        automaticallyAdjustContentInsets={false}
+      />
     );
   }
 });
 
 var styles = StyleSheet.create({
+  feedCard: {
+    flexDirection: 'row',
+    margin: 10,
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowRadius: 1,
+    shadowOffset: {
+      h: 1
+    },
+    shadowOpacity: 0.1
+  },
+  productImage: {
+    width: 120,
+    height: 160
+  },
+  fullScreen: {
+    flex: 1
+  },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 64,
+    paddingBottom: 64,
+    backgroundColor: '#EEEEEE',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    
+  },
+  tags: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    // marginTop: 5,
+    marginBottom: 20
   },
   instructions: {
+    fontSize: 13,
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    height: 120
   },
 });
 
